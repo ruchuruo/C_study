f3756b3ba0ecf1a55bfa8ccb18a2f9c6914c553e
ruchuruo <ruchuruo@DESKTOP-I0JD1J7>
2024-11-29 03:00:08 +0800

    2024年11月29日02点59分

--- CPP-图形库-EasyX/my_h.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define easyx_h "EasyX_2023¥Û Ó∞Ê/include/easyx.h"
-#define graphics_h "EasyX_2023¥Û Ó∞Ê/include/graphics.h"
---
+++ CPP_Network/1-tcp网络要学习哪些内容.cpp
@@ -0,0 +1,21 @@
+/*
+	tcp网络要学习哪些内容
+
+	网络编程
+
+		通过代码网络收发数据
+
+		基于posix api, socket
+
+		select/poll/epoll, reactor,多线程,多进程如何做
+
+	网络原理
+
+		tcp三次握手,四次挥手,
+
+		滑动窗口,慢启动,拥塞控制,超时重传,延迟确认,
+
+		arp,icmp,igmp,tcpdump/wireshark,
+
+		fd分配机制,epoll,网关
+*/
